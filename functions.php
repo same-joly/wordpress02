592755c7cbadb4f36a219cf5e7509d2ad3998549
same <same.jolly2@gmail.com>
2021-08-19 21:05:11 +0900

    2021.08.19 commit.

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 function theme_setup() {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'custom-logo' );
+
+	register_nav_menus( array(
+		'global' => 'Global Menu'
+	) );
 }
 add_action( 'after_setup_theme', 'theme_setup' );
 
